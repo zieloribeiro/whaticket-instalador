4443f0befc17740a805396443f2afbf2bc36a1d5
Wilian Guilherme <wilian@aluno.ufrb.edu.br>
2023-03-31 00:06:35 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -35,7 +35,7 @@ system_git_clone() {
   sleep 2
 
   sudo su - deploy <<EOF
-  git clone https://github.com/ricardomecca/whaticket /home/deploy/whaticket/
+  git clone https://github.com/wiliangui/owenzap-main /home/deploy/whaticket/
 EOF
 
   sleep 2
